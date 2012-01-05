47ad1bd0e46386326e5192fe3d8b1fb3b356ca34
akhil stanislavose <akhil@mobme.in>
2012-01-05 15:27:52 +0530

    Implemneted filling of water

--- lib/hitting_rock_bottom.rb
+++ lib/hitting_rock_bottom.rb
@@ -1,2 +1,37 @@
 class HittingRockBottom
+
+  attr_accessor :cave, :units, :last_unit
+
+  def initialize path_to_cave_file
+    File.open path_to_cave_file, "r" do |file|
+      @cave = file.readlines
+    end
+    @units = @cave.shift(2).first.to_i - 1
+    @last_unit = { :col => 0, :row => 1 }
+  end
+
+  def next_position arg_cave=nil, arg_last_unit=nil
+    cave = arg_cave || @cave
+    last_unit = arg_last_unit || @last_unit
+    if cave[last_unit[:row]+1][last_unit[:col]] == " "
+      last_unit[:row] += 1
+    elsif cave[last_unit[:row]][last_unit[:col]+1] == " "
+      last_unit[:col] += 1
+    elsif cave[last_unit[:row]-1][last_unit[:col]] == " "
+      last_unit[:row] -= 1
+      last_unit[:col] = cave[last_unit[:row]].index ' '
+    end
+    last_unit
+  end
+
+  def fill_water
+    while @units > 0 do
+      next_unit = next_position
+      @cave[next_unit[:row]][next_unit[:col]] = "~"
+      @last_unit = next_unit
+      @units -= 1
+    end
+    @cave
+  end
+
 end
--- spec/hitting_rock_bottom_spec.rb
+++ spec/hitting_rock_bottom_spec.rb
@@ -1,5 +1,93 @@
 require 'spec_helper'
 require 'hitting_rock_bottom'
+require 'pp'
 
 describe HittingRockBottom do
+
+  before :each do
+    @o = HittingRockBottom.new(File.expand_path(File.dirname(__FILE__)) + '/fixtures/simple_cave.txt')
+  end
+
+  it "should load the cave from file on initalization" do
+
+    @o.cave.should == [
+      "################################\n",
+      "~                              #\n",
+      "#         ####                 #\n",
+      "###       ####                ##\n",
+      "###       ####              ####\n",
+      "#######   #######         ######\n",
+      "#######   ###########     ######\n",
+      "################################\n"
+    ]
+
+    @o.units.should == 44
+
+  end
+
+  it "should find next postion for water" do
+
+    cave1_last_pos = { :col => 0, :row => 1 }
+    cave1 = [
+      "################################\n",
+      "~                              #\n",
+      "#         ####                 #\n",
+      "###       ####                ##\n",
+      "###       ####              ####\n",
+      "#######   #######         ######\n",
+      "#######   ###########     ######\n",
+      "################################\n"
+    ]
+
+    @o.next_position(cave1,cave1_last_pos).should == { :col => 1, :row => 1 }
+
+
+    cave2_last_pos = { :col => 1, :row => 1 }
+    cave2 = [
+      "################################\n",
+      "~~                             #\n",
+      "#         ####                 #\n",
+      "###       ####                ##\n",
+      "###       ####              ####\n",
+      "#######   #######         ######\n",
+      "#######   ###########     ######\n",
+      "################################\n"
+    ]
+
+    @o.next_position(cave2,cave2_last_pos).should == { :col => 1, :row => 2 }
+
+
+    cave3_last_pos = { :col => 9, :row => 6 }
+    cave3 = [
+      "################################\n",
+      "~~                             #\n",
+      "#~        ####                 #\n",
+      "###~      ####                ##\n",
+      "###~~~~~  ####              ####\n",
+      "#######~  #######         ######\n",
+      "#######~~~###########     ######\n",
+      "################################\n"
+    ]
+
+    @o.next_position(cave3,cave3_last_pos).should == { :col => 8, :row => 5 }
+
+  end
+
+  it "should fill water properly" do
+
+    @o.fill_water.should == [
+      "################################\n",
+      "~~~~~~~~~~~~~~~                #\n",
+      "#~~~~~~~~~####~                #\n",
+      "###~~~~~~~####                ##\n",
+      "###~~~~~~~####              ####\n",
+      "#######~~~#######         ######\n",
+      "#######~~~###########     ######\n",
+      "################################\n"
+    ]
+
+  end
+
+
+
 end
